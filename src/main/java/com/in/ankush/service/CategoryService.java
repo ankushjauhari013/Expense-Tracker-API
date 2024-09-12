65b914316fb521fbba15f05b873b472903d721eb
ANKUSH JAUHARI <77112058+ankushjauhari013@users.noreply.github.com>
2024-09-13 00:10:01 +0530

    CategoryService.java

--- src/main/java/com/in/ankush/service/CategoryService.java
+++ src/main/java/com/in/ankush/service/CategoryService.java
@@ -1,14 +1,33 @@
 package com.in.ankush.service;
 
 import java.util.List;
-
 import com.in.ankush.dto.CategoryDto;
 
+
+/*  
+ * Service interface for managing the categories 
+ * @author Ankush Jauhari
+ * */
+
 public interface CategoryService {
-	
+
+/*  
+ * This is for reading the categories from database	 
+ * @return list
+ * */
 	List<CategoryDto> getAllCategories();
 
+/*  
+ * This is for creating the new category	 
+ * @param categoryDTO
+ * @return categoryDTO
+ * */
 	CategoryDto saveCategory(CategoryDto categoryDto);
-	
+
+/*  
+ * This is for deleting the category from database	 
+ * @param categoryId
+ * @return void
+ * */
 	void deleteCategory(String categoryId);
 }
