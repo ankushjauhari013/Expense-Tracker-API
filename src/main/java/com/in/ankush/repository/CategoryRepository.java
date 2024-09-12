feb67fc28fb9b6257efc8645305247298a864f99
ANKUSH JAUHARI <77112058+ankushjauhari013@users.noreply.github.com>
2024-09-13 00:37:23 +0530

    CategoryRepository.java

--- src/main/java/com/in/ankush/repository/CategoryRepository.java
+++ src/main/java/com/in/ankush/repository/CategoryRepository.java
@@ -2,15 +2,23 @@ package com.in.ankush.repository;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.in.ankush.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long>{
 
+/*  
+ * finder method to retrieve the categories by userId
+ * @param userId
+ * @return list
+ * */
 	List<CategoryEntity>findByUserId(Long userId);
-	
+
+/*  
+ * finder method to fetch the category by userId and categoryId  
+ * @param id, categoryId
+ * @return Optional<categoryEntity>
+ * */
 	Optional<CategoryEntity> findByUserIdAndCategoryId(Long id, String categoryId);
 
 }
